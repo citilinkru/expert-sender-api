c5eda90fdbefffb0dfa3721563ed5afe579163c7
Dawid Nowak <contact@dnowak.pl>
2014-04-16 19:24:54 +0200

    ExpertSenderEnum: added ::getTypes(), ::getOperators(), ::getOrders(), ::getModes() methods

--- src/LinguaLeo/ExpertSender/ExpertSenderEnum.php
+++ src/LinguaLeo/ExpertSender/ExpertSenderEnum.php
@@ -22,4 +22,36 @@ class ExpertSenderEnum
     const MODE_ADD_AND_IGNORE = 'AddAndIgnore';
     const MODE_IGNORE_AND_UPDATE = 'IgnoreAndUpdate';
     const MODE_IGNORE_AND_REPLACE = 'IgnoreAndReplace';
+
+    /**
+     * @return array
+     */
+    public static function getTypes()
+    {
+        return [self::TYPE_BOOLEAN, self::TYPE_STRING, self::TYPE_INTEGER, self::TYPE_DATE, self::TYPE_DATE_TIME];
+    }
+
+    /**
+     * @return array
+     */
+    public static function getOperators()
+    {
+        return [self::OPERATOR_EQUALS, self::OPERATOR_GREATER, self::OPERATOR_LOWER, self::OPERATOR_LIKE];
+    }
+
+    /**
+     * @return array
+     */
+    public static function getOrders()
+    {
+        return [self::ORDER_ASCENDING, self::ORDER_DESCENDING];
+    }
+
+    /**
+     * @return array
+     */
+    public static function getModes()
+    {
+        return [self::MODE_ADD_AND_UPDATE, self::MODE_ADD_AND_REPLACE, self::MODE_ADD_AND_IGNORE, self::MODE_IGNORE_AND_UPDATE, self::MODE_IGNORE_AND_REPLACE];
+    }
 }
