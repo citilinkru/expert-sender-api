f086e0a7fb6307c983ad5b5dcc89907fee67377d
Nikita Sapogov <amstaffix@gmail.com>
2017-08-05 18:42:17 +0300

    add more information in RemovedSubscriberGetRequest constructor

--- src/Request/RemovedSubscriberGetRequest.php
+++ src/Request/RemovedSubscriberGetRequest.php
@@ -7,6 +7,7 @@ use Citilink\ExpertSenderApi\Enum\HttpMethod;
 use Citilink\ExpertSenderApi\Enum\RemovedSubscribersGetRequest\Option;
 use Citilink\ExpertSenderApi\Enum\RemovedSubscribersGetRequest\RemoveType;
 use Citilink\ExpertSenderApi\RequestInterface;
+use Webmozart\Assert\Assert;
 
 /**
  * Request for GET RemoveSubscribers
@@ -67,11 +68,15 @@ class RemovedSubscriberGetRequest implements RequestInterface
     /**
      * Constructor
      *
-     * @param int[] $listIds List IDs
-     * @param string[] $removeTypes Remove types (reasons)
-     * @param \DateTime $startDate End date
-     * @param \DateTime $endDate Start date
-     * @param Option $option Option
+     * @param int[] $listIds List IDs. If specified, only removed subscribers from given lists will be returned. If
+     *      not specified, removed subscribers from all lists will be returned
+     * @param RemoveType[] $removeTypes Remove types (reasons). If specified, only subscribers removed for listed
+     *      reasons will be returned. If omitted, all reasons will be included
+     * @param \DateTime $startDate End date. If specified, subscribers removed prior to this date will not be returned.
+     *      May be used together with endDate to specify a period of time
+     * @param \DateTime $endDate Start date. If specified, subscribers removed after this date will not be returned.
+     *      May be used together with startDate to specify a period of time
+     * @param Option $option Option. If specified, additional subscriber information will be returned
      */
     public function __construct(
         array $listIds = [],
@@ -80,6 +85,8 @@ class RemovedSubscriberGetRequest implements RequestInterface
         \DateTime $endDate = null,
         Option $option = null
     ) {
+        Assert::allInteger($listIds);
+        Assert::allIsInstanceOf($removeTypes, RemoveType::class);
         $this->listIds = $listIds;
         $this->removeTypes = $removeTypes;
         $this->startDate = $startDate;
