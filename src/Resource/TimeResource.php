b6f4e11a862dff69a6ff72b386a8145532c0e85f
Nikita Sapogov <amstaffix@gmail.com>
2017-08-06 00:52:52 +0300

    change method name in TimeResource

--- src/Resource/TimeResource.php
+++ src/Resource/TimeResource.php
@@ -19,7 +19,7 @@ class TimeResource extends AbstractResource
      *
      * @return TimeGetResponse Time response
      */
-    public function getTime(): TimeGetResponse
+    public function get(): TimeGetResponse
     {
         return new TimeGetResponse($this->requestSender->send(new TimeGetRequest()));
     }
