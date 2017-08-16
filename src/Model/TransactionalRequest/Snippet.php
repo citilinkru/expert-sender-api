b49661c3f011fe3aa4597d51e132c804e9cf8f72
Nikita Sapogov <amstaffix@gmail.com>
2017-08-16 12:52:19 +0300

    added class to not break compatibility

---
+++ src/Model/TransactionalRequest/Snippet.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Citilink\ExpertSenderApi\Model\TransactionalRequest;
+
+/**
+ * Snippet
+ *
+ * @deprecated use {@see \Citilink\ExpertSenderApi\Model\TransactionalPostRequest\Snippet} instead
+ *
+ * @author Nikita Sapogov <sapogov.n@citilink.ru>
+ */
+class Snippet extends \Citilink\ExpertSenderApi\Model\TransactionalPostRequest\Snippet
+{
+
+}
